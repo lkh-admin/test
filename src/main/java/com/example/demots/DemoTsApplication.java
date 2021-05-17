cf8f4809ff47a035b3c504624cff48ba2b13a181
kaihel <895421405@qq.com>
2021-05-17 20:04:49 +0800

    首次提交11

---
+++ src/main/java/com/example/demots/DemoTsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DemoTsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demots/DemoTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
